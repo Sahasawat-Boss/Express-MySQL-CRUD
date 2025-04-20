b96d082287b229045a4b18101d7062648483025f
Sahasawat-Boss <Sahasawat.rk@gmail.com>
2025-04-20 18:10:49 +0700

    a API Get all students

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,4 +1,31 @@
+const db = require("../config/db");
+
 //GET ALL STUDENT LIST
-const getStudents = () => { }
+const getStudents = async (req, res) => {
+    try {
+        const data = await db.query(' SELECT * FROM students')
+        if(!data){
+            return res.status(404).send({
+                sucess: false,
+                message: 'No record found',
+            });
+        }
+
+        res.status(200).send({
+            sucess: true,
+            message: 'All Student Record from Student DB',
+            totalStudents: data[0].length,
+            data: data[0],
+        });
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            sucess: false,
+            message: 'ERROR on GET all student API',
+            error
+        });
+    }
+}
 
 module.exports = { getStudents };
